5fe4a63bec647b986644e2721f0aa18767b1fba1
aaron-phoenix <aaron-phoenix@yandex.ru>
2025-10-30 17:46:45 +0300

    lesson1 (#4)
    * 02_lesson
    
    * Удаление лишних файлов
    
    * lesson3_commit
    
    * new lesson3 commit

---
+++ 03_lesson/lesson_3_task_1.py
@@ -0,0 +1,7 @@
+from user import User
+
+my_user = User("Елена", "Золотарева")
+
+print(my_user.get_firstname())
+print(my_user.get_lastname())
+print(my_user.get_name_info())
---
+++ 03_lesson/smartphone.py
@@ -0,0 +1,5 @@
+class Smartphone:
+    def __init__(self, phone_mark, phone_model, phone_number):
+        self.phone_mark = phone_mark
+        self.phone_model = phone_model
+        self.phone_number = phone_number

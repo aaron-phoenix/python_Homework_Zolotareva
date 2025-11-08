fdd8a01963bec835672f07002ffe935144851a70
aaron-phoenix <aaron-phoenix@yandex.ru>
2025-11-08 18:01:05 +0300

    Lesson4 (#5)
    * Lesson_04
    
    * 04_lesson

--- 03_lesson/smartphone.py
+++
@@ -1,5 +0,0 @@
-class Smartphone:
-    def __init__(self, phone_mark, phone_model, phone_number):
-        self.phone_mark = phone_mark
-        self.phone_model = phone_model
-        self.phone_number = phone_number

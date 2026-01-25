78a248c7a8b7a4e1b2bd1658b4cf9de5ecd699a2
aaron-phoenix <aaron-phoenix@yandex.ru>
2026-01-25 20:38:57 +0300

    lesson9M

--- 09_lesson/test_database.py
+++ 09_lesson/test_database.py
@@ -1,16 +1,15 @@
 from sqlalchemy import create_engine, inspect, text
-
-db_connection_string = "postgresql://postgres:Xthnjdfext,f2025@localhost:5432/QA1"
-db = create_engine(db_connection_string)
+connection_string = "postgresql://postgres:Xthnjdfext,f2025@localhost:5432/QA1"
+__db = create_engine(connection_string)
 
 def test_db_connection():
-    inspector = inspect(db)
+    inspector = inspect(__db)
     first_student = inspector.get_table_names()
-    print(first_student[2])
-    assert first_student[2] == 'student'
+    print(first_student[0])
+    assert first_student[0] == 'users'
 
 def test_select():
-    connection = db.connect()
+    connection = __db.connect()
     result = connection.execute(text("SELECT * FROM users"))
     rows = result.mappings().all()
     row1 = rows[0]
@@ -19,16 +18,15 @@ def test_select():
     connection.close()
 
 def test_insert():
-    connection = db.connect()
-    transaction = connection.begin()
-    sql = "INSERT INTO users (\"user_email\") VALUES (:new_user_email)"
-    connection.execute(text(sql), {"new_user_email" : "aaron-phoenix@mail.ru"})
-    transaction.commit()
-    print("Данные успешно добавлены")
+    connection = __db.connect()
+    connection.begin()
+    sql = text("INSERT INTO users (\"user_email\") VALUES (:new_user_email)")
+    connection.execute(sql, {"new_user_email" : "aaron-phoenix@mail.ru"})
+    
     connection.close()
-
+    
 def test_update():
-    connection = db.connect()
+    connection = __db.connect()
     transaction = connection.begin()
 
     sql = text("UPDATE users SET user_id = :New_user_id WHERE user_email = :user_email")
@@ -39,7 +37,7 @@ def test_update():
     connection.close()
 
 def test_delete():
-    connection = db.connect()
+    connection = __db.connect()
     transaction = connection.begin()
 
     sql = text("DELETE FROM users WHERE user_id = :id")
